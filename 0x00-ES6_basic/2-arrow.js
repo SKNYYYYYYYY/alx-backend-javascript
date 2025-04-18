4d83f560e02e578d41dec84e6eb3f5554d0d4e7e
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2025-04-18 21:39:00 +0300

    arrow function main

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,9 +1,9 @@
 export default function getNeighborhoodlist() {
-  this.sanFransciscoNeighborhoods = ['SOMA', 'Union Square'];
-  
+  this.sanFransciscoNeighborhoods = ["SOMA", "Union Square"];
+
   const self = this;
   this.addNeighborhood = (newNeighborhood) => {
     self.sanFransciscoNeighborhoods.push(newNeighborhood);
     return self.sanFransciscoNeighborhoods;
-  }
+  };
 }
