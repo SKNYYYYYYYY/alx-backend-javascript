17709ca6ba558ff22869ba2a91af89ce721875cf
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2025-06-14 19:16:40 +0300

    corrected lint issues main

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,34 +1,46 @@
 const fs = require('fs');
 
+let students;
 const countStudents = (filename) => {
   if (!fs.existsSync(filename)) {
     throw new Error('Cannot load the database');
   }
-
   try {
     const data = fs.readFileSync(filename, 'utf-8');
     const lines = data.split('\n').filter((line) => line.trim());
 
     const keys = lines[0].split(',');
-    const values = lines.slice(1).map((line) => line.split(','));
-
-    const students = values.map((row) => Object.fromEntries(keys.map((key, i) => [key.trim(),
-      row[i].trim()])));
-
-    console.log(`Number of students: ${students.length}`);
-
-    const fields = {};
-    for (const student of students) {
-      const { field } = student;
-      fields[field] = (fields[field] || 0) + 1;
+    const values = [];
+    for (let i = 1; i < lines.length; i += 1) {
+      const details = lines[i].split(',');
+      values.push(details);
     }
-
-    for (const [field, count] of Object.entries(fields)) {
-      const names = students.filter((s) => s.field === field).map((s) => s.firstname).join(', ');
-      console.log(`Number of students in ${field}: ${count}. List: ${names}`);
-    }
+    students = values.map((row) => Object.fromEntries(keys.map((key, i) => [key, row[i]])));
   } catch (err) {
-    throw new Error('Cannot load the database');
+    console.log(err);
+  }
+
+  const totalStudents = students.length;
+  console.log(`Number of students: ${totalStudents}`);
+
+  const fields = {};
+  for (let i = 0; i < students.length; i += 1) {
+    const { field } = students[i];
+    fields[field] = (fields[field] || 0) + 1;
+  }
+
+  const fieldStudents = (field) => {
+    const list = [];
+    for (const student of students) {
+      if (student.field === field) {
+        list.push(student.firstname);
+      }
+    }
+    return list.join(', ');
+  };
+
+  for (const [key, value] of Object.entries(fields)) {
+    console.log(`Number of students in ${key}: ${value}. List: ${fieldStudents(key)}`);
   }
 };
 
