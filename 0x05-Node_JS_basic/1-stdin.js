51e79f18289e4f75b2412b6dd73c3f2256ed0858
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2025-06-14 18:08:36 +0300

    corrected lint issues main

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,17 +1,11 @@
-const readline = require('readline');
+process.stdout.write('Welcome to ALX, what is your name?\n');
 
-console.log('Welcome to ALX, what is your name?');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdin.isTTY ? process.stdout : undefined,
-});
-
-rl.on('line', (input) => {
-  console.log(`Your name is: ${input}`);
-  rl.close();
-
-  if (!process.stdin.isTTY) {
-    console.log('This important software is now closing');
+process.stdin.on('readable', () => {
+  const input = process.stdin.read();
+  if (input != null) {
+    process.stdout.write(`Your name is: ${input}`);
   }
+});
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
