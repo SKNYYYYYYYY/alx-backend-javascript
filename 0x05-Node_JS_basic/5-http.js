eaea1491398840abf9e6ec1fbbca27652841f475
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2025-06-14 20:59:08 +0300

    create a HTTP server to fetch and clean data from db main

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,58 @@
+const res = require('express/lib/response');
+const fs = require('fs');
+
+const http = require('http');
+
+let dbStudents;
+function students(filename) {
+  if (!fs.existsSync(filename)) {
+    throw new Error('Cannot load the database');
+  }
+  const data = fs.readFileSync(filename, 'utf-8');
+  const lines = data.split('\n').filter((line) => line.trim());
+  const keys = lines[0].split(',');
+  const values = [];
+  for (let i = 1; i < lines.length; i += 1) {
+    const details = lines[i].split(',');
+    values.push(details);
+  }
+  dbStudents = values.map((row) => Object.fromEntries(keys.map((key, i) => [key, row[i]])));
+  const totalStudents = dbStudents.length;
+  const response = [];
+  response.push(`Number of students: ${totalStudents}`);
+
+  const fields = {};
+  for (let i = 0; i < dbStudents.length; i += 1) {
+    const { field } = dbStudents[i];
+    fields[field] = (fields[field] || 0) + 1;
+  }
+
+  const fieldStudents = (field) => {
+    const list = [];
+    for (const student of dbStudents) {
+      if (student.field === field) {
+        list.push(student.firstname);
+      }
+    }
+    return list.join(', ');
+  };
+
+  for (const [key, value] of Object.entries(fields)) {
+    response.push(`Number of students in ${key}: ${value}. List: ${fieldStudents(key)}`);
+  }
+  return response;
+}
+
+const app = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    const filename = process.argv[2];
+    const data = students(filename);
+    data.unshift('This is the list of our students');
+    console.log(data.join('\n'));
+    res.end(data.join('\n'));
+  }
+}).listen(1245);
+
+module.exports = app;
--- 0x05-Node_JS_basic/demo.js
+++ 0x05-Node_JS_basic/demo.js
@@ -1,53 +1,8 @@
-const { spawn } = require('node:child_process');
-
-const ls = spawn('ls');
-
-const fs = require('fs');
-
-let students;
-const countStudents = (filename) => {
-  ls.stdout.on('data', (data) => {
-    const files = data.toString().split('\n');
-    if (files.includes(filename)) {
-      try {
-        let stdout = fs.readFileSync(filename);
-        stdout = stdout.toString().split('\n').filter((line) => line.trim());
-        const keys = stdout[0].split(',');
-        const values = [];
-        for (let i = 1; i < stdout.length; i += 1) {
-          const details = stdout[i].split(',');
-          values.push(details);
-        }
-        students = values.map((row) => Object.fromEntries(keys.map((key, i) => [key, row[i]])));
-      } catch (err) {
-        console.log(err);
-      }
-
-      const totalStudents = students.length;
-      console.log(`Number of students: ${totalStudents}`);
-
-      const fields = {};
-      for (let i = 0; i < students.length; i += 1) {
-        const { field } = students[i];
-        fields[field] = (fields[field] || 0) + 1;
-      }
-
-      const fieldStudents = (field) => {
-        const list = [];
-        for (const student of students) {
-          if (student.field === field) {
-            list.push(student.firstname);
-          }
-        }
-        return list.join(', ');
-      };
-
-      for (const [key, value] of Object.entries(fields)) {
-        console.log(`Number of students in ${key}: ${value}. List: ${fieldStudents(key)}`);
-      }
-    } else {
-      throw new Error('Cannot load the database');
-    }
-  });
-};
-module.exports = countStudents;
+let myArray = [
+  'This is the list of our students',
+  'Number of students: 1',
+  'Number of students in CS: 6. List: Johann, Arielle, Jonathan, Emmanuel, Guillaume, Katie',
+  'Number of students in SWE: 4. List: Guillaume, Joseph, Paul, Tommy',
+];
+myArray = myArray.join(',');
+console.log(myArray);
