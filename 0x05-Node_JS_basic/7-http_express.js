e1de205c9ee5f42a069b8fcbd60fec4fa89010f8
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2025-06-14 21:50:02 +0300

    create a HTTP server to fetch and clean data from db main

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,69 @@
+const fs = require('fs');
+
+const express = require('express');
+
+let dbStudents;
+function students(filename) {
+  if (!fs.existsSync(filename)) {
+    return ('Cannot load the database');
+    // throw new Error('Cannot load the database');
+  }
+  const data = fs.readFileSync(filename, 'utf-8');
+  const lines = data.split('\n').filter((line) => line.trim());
+  const keys = lines[0].split(',');
+  const values = [];
+  for (let i = 1; i < lines.length; i += 1) {
+    const details = lines[i].split(',');
+    values.push(details);
+  }
+  dbStudents = values.map((row) => Object.fromEntries(keys.map((key, i) => [key, row[i]])));
+  const totalStudents = dbStudents.length;
+  const response = [];
+  response.push(`Number of students: ${totalStudents}`);
+
+  const fields = {};
+  for (let i = 0; i < dbStudents.length; i += 1) {
+    const { field } = dbStudents[i];
+    fields[field] = (fields[field] || 0) + 1;
+  }
+
+  const fieldStudents = (field) => {
+    const list = [];
+    for (const student of dbStudents) {
+      if (student.field === field) {
+        list.push(student.firstname);
+      }
+    }
+    return list.join(', ');
+  };
+
+  for (const [key, value] of Object.entries(fields)) {
+    response.push(`Number of students in ${key}: ${value}. List: ${fieldStudents(key)}`);
+  }
+  return response;
+}
+const app = express();
+
+app.get('/', (req, res) => {
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+  }
+});
+
+app.get('/students', (req, res) => {
+  try {
+    const filename = process.argv[2];
+    const data = students(filename);
+    if (typeof (data) === 'object') {
+      data.unshift('This is the list of our students');
+    }
+    res.end(data.join('\n'));
+  } catch (err) {
+    res.statusCode = 404;
+    res.end('This is the list of our students\nCannot load the database');
+  }
+});
+
+app.listen(1245);
+
+module.exports = app;
