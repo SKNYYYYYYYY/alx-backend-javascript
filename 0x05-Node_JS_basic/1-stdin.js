947e63807be56390b50a998565370cb30a72a3a4
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2025-06-14 17:44:27 +0300

    corrected lint issues main

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,7 +2,9 @@ console.log('Welcome to ALX, what is your name?');
 
 process.stdin.on('readable', () => {
   const input = process.stdin.read();
-  process.stdout.write(`Your name is: ${input}`);
+  if (input != null) {
+    process.stdout.write(`Your name is: ${input}`);
+  }
 });
 process.stdin.on('end', () => {
   console.log('This important software is now closing');
--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,55 +1,54 @@
-const {spawn, execFileSync} = require('node:child_process')
-const ls = spawn('ls')
-const fs = require("fs")
+const { spawn } = require('node:child_process');
 
+const ls = spawn('ls');
+
+const fs = require('fs');
+
+let students;
 function countStudents(filename) {
-    ls.stdout.on('data', (data) => {
-        const files = data.toString().split('\n')
-        if (files.includes(filename)){
-            try{
-                var stdout = fs.readFileSync(filename)
-                stdout = stdout.toString().split("\n").filter(line => line.trim())
-                const keys = stdout[0].split(",")
-                var values = []
-                for(let i=1; i <stdout.length; i++) {
-                    const details = stdout[i].split(',')
-                    values.push(details)
-                }
-                var students = values.map(row => Object.fromEntries(keys.map((key, i) => [key, row[i]])))
-            }
-            catch(err) {
-                console.log("" + err)
-            }
-
-            const total_students = students.length
-            console.log("Number of students: " + total_students)
-
-            var fields = {}
-            for(let i=0; i<students.length;i++){
-                const field = students[i]["field"]
-                fields[field] = (fields[field] || 0) + 1
-            }
-            const CS_num = fields['CS']
-            const SWE_num = fields['SWE']
-
-            const field_students = (field) => {
-                var list = []
-                for (let student of students){
-                    if (student["field"] == field){
-                        list.push(student["firstname"])
-                    }
-                }
-                return list.join(', ')
-                }
-
-
-            console.log("Number of students in CS: " + CS_num + ". " + "List: " + field_students("CS"))
-            console.log("Number of students in SWE: " + SWE_num + ". " + "List: " + field_students("SWE"))
-
+  ls.stdout.on('data', (data) => {
+    const files = data.toString().split('\n');
+    if (files.includes(filename)) {
+      try {
+        let stdout = fs.readFileSync(filename);
+        stdout = stdout.toString().split('\n').filter((line) => line.trim());
+        const keys = stdout[0].split(',');
+        const values = [];
+        for (let i = 1; i < stdout.length; i += 1) {
+          const details = stdout[i].split(',');
+          values.push(details);
         }
-        else{
-            throw new Error('Cannot load the database')
+        students = values.map((row) => Object.fromEntries(keys.map((key, i) => [key, row[i]])));
+      } catch (err) {
+        console.log(err);
+      }
+
+      const totalStudents = students.length;
+      console.log(`Number of students: ${totalStudents}`);
+
+      const fields = {};
+      for (let i = 0; i < students.length; i += 1) {
+        const { field } = students[i];
+        fields[field] = (fields[field] || 0) + 1;
+      }
+      const csNum = fields.CS;
+      const sweNum = fields.SWE;
+
+      const fieldStudents = (field) => {
+        const list = [];
+        for (const student of students) {
+          if (student.field === field) {
+            list.push(student.firstname);
+          }
         }
-    })
+        return list.join(', ');
+      };
+
+      console.log(`Number of students in CS: ${csNum}. List: ${fieldStudents('CS')}`);
+      console.log(`Number of students in SWE: ${sweNum}. List: ${fieldStudents('SWE')}`);
+    } else {
+      throw new Error('Cannot load the database');
+    }
+  });
 }
-module.exports = countStudents
+module.exports = countStudents;
