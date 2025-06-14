8cc48a9fd3e114905f4a78d96c1ee07413685e76
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2025-06-14 19:55:43 +0300

    create a simple HTTP server main

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,8 +1,11 @@
 const http = require('http');
 
-const app = http.createServer((request, response) => {
-  response.statusCode = 200;
-  response.end('Hello ALX!');
-}).listen(1245);
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello ALX!');
+});
+
+app.listen(1245);
 
 module.exports = app;
