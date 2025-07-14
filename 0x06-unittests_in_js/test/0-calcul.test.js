281c8df1377673bfe29998921b4258650a97ff99
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2025-07-14 17:27:33 +0300

    test calculatenumber function main

--- 0x06-unittests_in_js/test/0-calcul.test.js
+++ 0x06-unittests_in_js/test/0-calcul.test.js
@@ -4,9 +4,9 @@ const { calculatenumber } = require('../0-calcul');
 
 describe('calculatenumber', function() {
   it('should return 6 when 2 and 4 passed to it', function() {
-    assert.equal(calculatenumber(2,4), 6)
+    assert.strictEqual(calculatenumber(2,4), 6)
   })
   it('should return -3 when 2 and -5 passed to it', function() {
-    assert.equal(calculatenumber(2,-5), -3)
+    assert.strictEqual(calculatenumber(2,-5), -3)
   })
 })
