d8a4610bad6957a8c24af1728087a9e3f173f24a
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2025-07-21 17:14:56 +0300

    feat: Implement hooks main

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,35 @@
+/* eslint-disable no-unused-expressions */
+/* eslint-disable jest/valid-expect */
+/* eslint-disable jest/expect-expect */
+const { describe, afterEach } = require('mocha');
+const sendPaymentRequestToApi = require('./3-payment');
+const Utils = require('./utils');
+const sinon = require('sinon');
+const { expect } = require('chai');
+
+describe('sendPaymentRequestToApi', () => {
+  afterEach(() => {
+    sinon.restore();
+  });
+  it('should call sendPaymentRequestToApi with 100 and 20 and spy on console.log to check if correct message is logged', () => {
+    const spy = sinon.spy(console, 'log');
+
+    const type = 'SUM';
+    const a = 100;
+    const b = 20;
+
+    sendPaymentRequestToApi(a, b);
+    expect(Utils.calculateNumber(type, a, b)).to.equal(120);
+
+    expect(spy.calledOnce).to.be.true;
+    expect(spy.calledWith('The total is: 120')).to.be.true;
+  });
+  it('should call sendPaymentRequestToAPI with 10, and 10', () => {
+    const spy = sinon.spy(console, 'log');
+    sendPaymentRequestToApi(10, 10);
+    expect(Utils.calculateNumber('SUM', 10, 10)).to.equal(20);
+
+    expect(spy.calledOnce).to.be.true;
+    expect(spy.calledWith('The total is: 20')).to.be.true;
+  });
+});
