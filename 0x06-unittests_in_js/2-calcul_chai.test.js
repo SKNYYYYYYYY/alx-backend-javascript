5292468c4faf4cbd6d0e867e5d14503df7a5f61c
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2025-07-21 13:03:01 +0300

    feat: use expect from chai instead of node assert

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -2,7 +2,7 @@
 const { describe } = require('mocha');
 
 const chai = require('chai');
-const calculateNumber = require('./1-calcul');
+const calculateNumber = require('./2-calcul_chai');
 
 const { expect } = chai;
 
