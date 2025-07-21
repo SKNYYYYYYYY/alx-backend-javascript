830aa66faff76e50f4a562a1ca56fac6903dce77
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2025-07-21 11:32:26 +0300

    feat: use expect from chai instead of node assert

---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,22 @@
+/* eslint-disable jest/expect-expect */
+const { describe } = require('mocha');
+
+const chai = require('chai');
+const calculateNumber = require('./1-calcul');
+
+const { expect } = chai;
+
+describe('calculateNumber', () => {
+  it('should return 2 when type is SUM', () => {
+    expect(calculateNumber('SUM', 1, 1)).to.equal(2);
+  });
+  it('should return 2 when type is SUBTRACT', () => {
+    expect(calculateNumber('SUBTRACT', 4, 2)).to.equal(2);
+  });
+  it('should return 2 when type is DIVIDE', () => {
+    expect(calculateNumber('DIVIDE', 4, 2)).to.equal(2);
+  });
+  it('should return Error when type is DIVIDE and rounded b is 0', () => {
+    expect(calculateNumber('DIVIDE', 4, 0)).to.equal('Error');
+  });
+});
